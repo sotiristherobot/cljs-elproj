dd417a197b19562c75a98bbad47a6f6fb06e6c78
Sotiris Ioannou <sotiris@jsmate.net>
2020-04-30 00:49:57 +0200

    Some basic styling

--- src/frontend/home/core.cljs
+++ src/frontend/home/core.cljs
@@ -2,11 +2,11 @@
 
   )
 (defn side-bar [username]
-      [:div {:style {:margin "20px"}}
+      [:div {:style {:margin "20px" :display "flex" :maxWidth "350px" :flexGrow 1}}
        [:h4 (str "Hello " username)]])
 
 (defn home-container [state]
       (let [username (get @state :username)]
-           [:div {:style {:display "flex" :justify-content "center"}}
+           [:div {:style {:display "flex"}}
             [side-bar username]
-            [:div {:style {:margin "20px"}} [:h4 "Home Component"]]]))
+            [:div {:style {:margin "20px" :display "flex" :flexGrow 1}} [:h4 "Home Component"]]]))
