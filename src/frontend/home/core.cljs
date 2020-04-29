98eab7dc6c8b3622e811bf585123eec7a8375f77
Sotiris Ioannou <sotiris@jsmate.net>
2020-04-30 01:09:12 +0200

    Generate sidebar menu items

--- src/frontend/home/core.cljs
+++ src/frontend/home/core.cljs
@@ -1,9 +1,20 @@
-(ns frontend.home.core
+(ns frontend.home.core)
 
-  )
+
+(defn generate-menu-list [menu-items]
+      [:div
+       [:ul {:style {:paddingLeft "20px" :margin "0px"}}
+        (map (fn [item]
+                 [:li [:a item]])
+             menu-items)]])
+
 (defn side-bar [username]
-      [:div {:style {:margin "20px" :display "flex" :maxWidth "350px" :flexGrow 1}}
-       [:h4 (str "Hello " username)]])
+      [:div {:style {:margin "20px" :display "flex" :maxWidth "350px" :flexGrow 1
+                     :flexDirection "column"}}
+       [:h4 (str "Hello " username)]
+       [generate-menu-list ["Home" "About"]]])
+
+
 
 (defn home-container [state]
       (let [username (get @state :username)]
